6a1e35b3abcd33e354577b0e37ec11a4164320ec
Jan Willamowius <jan@willamowius.de>
2018-01-27 23:47:13 +0100

    IPv6 support, cleanup

--- check_h323.cxx
+++ check_h323.cxx
@@ -4,24 +4,39 @@
  *
  * License: GPL
  *
- * (c) Relaxed Communications GmbH, 2009-2017
+ * (c) Relaxed Communications GmbH, 2009-2018
  *     jan@willamowius.de, https://www.willamowius.com
  *
  */
 
 #include <ptlib.h>
 #include <ptlib/sockets.h>
-#include <ptclib/asner.h>
 #include "h225.h"
-#include "h323.h"
 #include "h323pdu.h"
 
-#ifdef H323_H450
-#include "h450/h4501.h"
-#include "h450/h450pdu.h"
-#endif // H323_H450
-
 const char H225_ProtocolID[] = "0.0.8.2250.0.2";
+
+// convert a socket IP address into an H225 transport address
+H225_TransportAddress SocketToH225TransportAddr(const PIPSocket::Address & Addr, WORD Port)
+{
+	H225_TransportAddress Result;
+
+	if (Addr.GetVersion() == 6) {
+		Result.SetTag(H225_TransportAddress::e_ip6Address);
+		H225_TransportAddress_ip6Address & ResultIP = Result;
+		for (int i = 0; i < 16; ++i)
+			ResultIP.m_ip[i] = Addr[i];
+		ResultIP.m_port = Port;
+	} else {
+		Result.SetTag(H225_TransportAddress::e_ipAddress);
+		H225_TransportAddress_ipAddress & ResultIP = Result;
+		for (int i = 0; i < 4; ++i)
+			ResultIP.m_ip[i] = Addr[i];
+		ResultIP.m_port = Port;
+	}
+
+	return Result;
+}
 
 class Client : public PProcess, public PUDPSocket
 {
@@ -30,8 +45,6 @@ protected:
 
     void SendGRQ();
     void SendLRQ();
-    void SendSetup();
-    void SendReleaseComplete();
 
     WORD gk_port;
     Address gk_addr, my_addr;
@@ -42,7 +55,7 @@ PCREATE_PROCESS(Client)
 void Client::Main()
 {
 	PArgList & args = GetArguments();
-	args.Parse("glsp:");
+	args.Parse("glp:");
 	if (args.GetCount() != 1) {
         cout << "Usage: check_h323 [-l|-g [-p port] host" << endl;
         exit(1);
@@ -62,9 +75,6 @@ void Client::Main()
 	freeaddrinfo(result);
 
     gk_port = 1719;
-    if (args.HasOption('s')) {
-    	gk_port = 1720;
-	}
     if (args.HasOption('p')) {
         gk_port = args.GetOptionString('p').AsUnsigned();
     }
@@ -76,27 +86,17 @@ void Client::Main()
 
     if (args.HasOption('l')) {
         SendLRQ();
-    } else if (args.HasOption('s')) {
-        SendReleaseComplete();
-//        SendSetup();
     } else {
         SendGRQ();
     }
 }
 
-void SetUUIE(Q931 & q931, const H225_H323_UserInformation & uuie)
-{
-    PPER_Stream strm;
-    uuie.Encode(strm);
-    strm.CompleteEncoding();
-    q931.SetIE(Q931::UserUserIE, strm);
-}
-
 struct TPKTV3 {
-        TPKTV3(WORD);
-        BYTE header, padding;
-        WORD length;
-    };
+    TPKTV3(WORD);
+    BYTE header;
+    BYTE padding;
+    WORD length;
+};
 
 TPKTV3::TPKTV3(WORD len)
     : header(3), padding(0)
@@ -106,97 +106,13 @@ TPKTV3::TPKTV3(WORD len)
 
 bool WriteTPKT(PTCPSocket & sock, const PBYTEArray & wtbuf)
 {
-    WORD len = (WORD)wtbuf.GetSize(), tlen = len + sizeof(TPKTV3);
+    WORD len = (WORD)wtbuf.GetSize();
+    WORD tlen = len + sizeof(TPKTV3);
     PBYTEArray tbuf(tlen);
     BYTE *bptr = tbuf.GetPointer();
     new (bptr) TPKTV3(len); // placement operator
     memcpy(bptr + sizeof(TPKTV3), wtbuf, len);
     return sock.Write(bptr, tlen);
-}
-
-void Client::SendReleaseComplete()
-{
-    Q931 q931;
-    H225_H323_UserInformation uuie;
-	q931.BuildReleaseComplete(1, true);
-	uuie.m_h323_uu_pdu.m_h323_message_body.SetTag(H225_H323_UU_PDU_h323_message_body::e_releaseComplete);
-	H225_ReleaseComplete_UUIE & rc = uuie.m_h323_uu_pdu.m_h323_message_body;
-	rc.m_protocolIdentifier.SetValue(H225_ProtocolID);
-    rc.m_callIdentifier.m_guid = OpalGloballyUniqueID();
-	SetUUIE(q931, uuie);
-
-	PBYTEArray rdbuf(20480), wtbuf;
-
-	q931.Encode(wtbuf);
-
-	PTCPSocket sock(gk_port);
-	if (!sock.Connect(gk_addr))
-	{
-		cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		exit(1);
-	}
-
-    if (!WriteTPKT(sock, wtbuf)) {
-		cout << "CRITICAL - Can not send to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		exit(1);
-    }
-
-    // TODO: check reply
-	cout << "OK - from " << gk_addr << endl;
-	exit(0);
-}
-
-void Client::SendSetup()
-{
-    Q931 q931;
-    H225_H323_UserInformation uuie;
-	q931.BuildSetup(1);
-	uuie.m_h323_uu_pdu.m_h323_message_body.SetTag(H225_H323_UU_PDU_h323_message_body::e_setup);
-	H225_Setup_UUIE & setup = uuie.m_h323_uu_pdu.m_h323_message_body;
-	setup.m_protocolIdentifier.SetValue(H225_ProtocolID);
-	setup.m_conferenceID = OpalGloballyUniqueID();
-    setup.m_callIdentifier.m_guid = setup.m_conferenceID;
-
-#ifdef H323_H450
-    // H.450
-    uuie.m_h323_uu_pdu.IncludeOptionalField(H225_H323_UU_PDU::e_h4501SupplementaryService);
-    H4501_SupplementaryService supplementaryService;
-    //H4501_InterpretationApdu & interpretation = supplementaryService.m_interpretationApdu;
-    supplementaryService.m_serviceApdu.SetTag(H4501_ServiceApdus::e_rosApdus);
-    PPER_Stream resultStream;
-    supplementaryService.Encode(resultStream);
-    resultStream.CompleteEncoding();
-    uuie.m_h323_uu_pdu.m_h4501SupplementaryService.SetSize(1);
-    uuie.m_h323_uu_pdu.m_h4501SupplementaryService[0].SetValue(resultStream) ;
-#else
-#warning("No H.450 support");
-#endif
-
-	SetUUIE(q931, uuie);
-
-	PBYTEArray rdbuf(20480), wtbuf;
-
-	q931.Encode(wtbuf);
-
-	PTCPSocket sock(gk_port);
-	if (!sock.Connect(gk_addr))
-	{
-		cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		exit(1);
-	}
-
-    if (!WriteTPKT(sock, wtbuf)) {
-		cout << "CRITICAL - Can not send to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		exit(1);
-    }
-
-    // TODO: check reply
-	cout << "OK - from " << gk_addr << endl;
-	exit(0);
 }
 
 void Client::SendGRQ()
@@ -208,14 +124,7 @@ void Client::SendGRQ()
 	grq.m_requestSeqNum.SetValue(1);
 	grq.m_protocolIdentifier.SetValue(H225_ProtocolID);
 
-	grq.m_rasAddress.SetTag(H225_TransportAddress::e_ipAddress);
-	H225_TransportAddress_ipAddress & ipAddress = grq.m_rasAddress;
-	ipAddress.m_ip[0] = my_addr.Byte1();
-	ipAddress.m_ip[1] = my_addr.Byte2();
-	ipAddress.m_ip[2] = my_addr.Byte3();
-	ipAddress.m_ip[3] = my_addr.Byte4();
-	ipAddress.m_port  = gk_port;
-
+	grq.m_rasAddress = SocketToH225TransportAddr(my_addr, gk_port);
 	grq.m_endpointType.IncludeOptionalField(grq.m_endpointType.e_terminal);
 
 	grq.IncludeOptionalField(grq.e_endpointAlias);
@@ -260,13 +169,7 @@ void Client::SendLRQ()
     lrq.m_requestSeqNum.SetValue(1);
 
 	// set replyAddress (mandatory)
-	lrq.m_replyAddress.SetTag(H225_TransportAddress::e_ipAddress);
-	H225_TransportAddress_ipAddress & ipAddress = lrq.m_replyAddress;
-	ipAddress.m_ip[0] = my_addr.Byte1();
-	ipAddress.m_ip[1] = my_addr.Byte2();
-	ipAddress.m_ip[2] = my_addr.Byte3();
-	ipAddress.m_ip[3] = my_addr.Byte4();
-	ipAddress.m_port  = gk_port;
+	lrq.m_replyAddress = SocketToH225TransportAddr(my_addr, gk_port);
 
     lrq.m_destinationInfo.SetSize(1);
     H323SetAliasAddress(PString("check_h323"), lrq.m_destinationInfo[0]);
@@ -304,4 +207,3 @@ void Client::SendLRQ()
 		exit(1);
 	}
 }
-

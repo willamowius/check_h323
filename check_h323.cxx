f1e337514f637b846cd8ef445bc02eca77178f82
Jan Willamowius <jan@willamowius.de>
2018-06-21 21:59:37 +0200

    whitespace

--- check_h323.cxx
+++ check_h323.cxx
@@ -19,23 +19,23 @@ const char H225_ProtocolID[] = "0.0.8.2250.0.2";
 // convert a socket IP address into an H225 transport address
 H225_TransportAddress SocketToH225TransportAddr(const PIPSocket::Address & Addr, WORD Port)
 {
-	H225_TransportAddress Result;
+    H225_TransportAddress Result;
 
-	if (Addr.GetVersion() == 6) {
-		Result.SetTag(H225_TransportAddress::e_ip6Address);
-		H225_TransportAddress_ip6Address & ResultIP = Result;
-		for (int i = 0; i < 16; ++i)
-			ResultIP.m_ip[i] = Addr[i];
-		ResultIP.m_port = Port;
-	} else {
-		Result.SetTag(H225_TransportAddress::e_ipAddress);
-		H225_TransportAddress_ipAddress & ResultIP = Result;
-		for (int i = 0; i < 4; ++i)
-			ResultIP.m_ip[i] = Addr[i];
-		ResultIP.m_port = Port;
-	}
+    if (Addr.GetVersion() == 6) {
+        Result.SetTag(H225_TransportAddress::e_ip6Address);
+        H225_TransportAddress_ip6Address & ResultIP = Result;
+        for (int i = 0; i < 16; ++i)
+            ResultIP.m_ip[i] = Addr[i];
+        ResultIP.m_port = Port;
+    } else {
+        Result.SetTag(H225_TransportAddress::e_ipAddress);
+        H225_TransportAddress_ipAddress & ResultIP = Result;
+        for (int i = 0; i < 4; ++i)
+            ResultIP.m_ip[i] = Addr[i];
+        ResultIP.m_port = Port;
+    }
 
-	return Result;
+    return Result;
 }
 
 class Client : public PProcess, public PUDPSocket
@@ -54,27 +54,27 @@ PCREATE_PROCESS(Client)
 
 void Client::Main()
 {
-	PArgList & args = GetArguments();
-	args.Parse("glp:");
-	if (args.GetCount() != 1) {
+    PArgList & args = GetArguments();
+    args.Parse("glp:");
+    if (args.GetCount() != 1) {
         cout << "Usage: check_h323 [-l|-g [-p port] host" << endl;
         _exit(1);
-	}
+    }
 
     PString hostname = args.GetParameter(0);
-	struct addrinfo hints;
-	struct addrinfo * result = NULL;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	if (getaddrinfo(hostname, NULL, &hints, &result) == 0) {
+    struct addrinfo hints;
+    struct addrinfo * result = NULL;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    if (getaddrinfo(hostname, NULL, &hints, &result) == 0) {
         gk_addr = PIPSocket::Address(result->ai_family, result->ai_addrlen, result->ai_addr);
-	} else {
-		cout << "CRITICAL - DNS lookup failed for " << hostname << endl;
-		_exit(1);
-	}
-	freeaddrinfo(result);
+    } else {
+        cout << "CRITICAL - DNS lookup failed for " << hostname << endl;
+        _exit(1);
+    }
+    freeaddrinfo(result);
 
-	// prefer IPv6 for own address if we connect to IPv6 destination
+    // prefer IPv6 for own address if we connect to IPv6 destination
     if (gk_addr.GetVersion() == 6)
         PIPSocket::SetDefaultIpAddressFamilyV6();
 
@@ -85,8 +85,8 @@ void Client::Main()
 
     GetHostAddress(my_addr);
 
-	// Read-Timeout
-	SetReadTimeout(PTimeInterval(3 * 1000));
+    // Read-Timeout
+    SetReadTimeout(PTimeInterval(3 * 1000));
 
     if (args.HasOption('l')) {
         SendLRQ();
@@ -121,47 +121,46 @@ bool WriteTPKT(PTCPSocket & sock, const PBYTEArray & wtbuf)
 
 void Client::SendGRQ()
 {
-	H225_RasMessage grq_ras, grq_rpl;
-	grq_ras.SetTag(H225_RasMessage::e_gatekeeperRequest);
-	H225_GatekeeperRequest & grq = grq_ras;
+    H225_RasMessage grq_ras, grq_rpl;
+    grq_ras.SetTag(H225_RasMessage::e_gatekeeperRequest);
+    H225_GatekeeperRequest & grq = grq_ras;
 
-	grq.m_requestSeqNum.SetValue(1);
-	grq.m_protocolIdentifier.SetValue(H225_ProtocolID);
+    grq.m_requestSeqNum.SetValue(1);
+    grq.m_protocolIdentifier.SetValue(H225_ProtocolID);
 
-	grq.m_rasAddress = SocketToH225TransportAddr(my_addr, gk_port);
-	grq.m_endpointType.IncludeOptionalField(grq.m_endpointType.e_terminal);
+    grq.m_rasAddress = SocketToH225TransportAddr(my_addr, gk_port);
+    grq.m_endpointType.IncludeOptionalField(grq.m_endpointType.e_terminal);
 
-	grq.IncludeOptionalField(grq.e_endpointAlias);
-	grq.m_endpointAlias.SetSize(1);
-	H323SetAliasAddress(PString("check_h323"), grq.m_endpointAlias[0]);
+    grq.IncludeOptionalField(grq.e_endpointAlias);
+    grq.m_endpointAlias.SetSize(1);
+    H323SetAliasAddress(PString("check_h323"), grq.m_endpointAlias[0]);
 
-	PBYTEArray rdbuf(2048), wtbuf(2048);
-	PPER_Stream rdstrm(rdbuf), wtstrm(wtbuf);
+    PBYTEArray rdbuf(2048), wtbuf(2048);
+    PPER_Stream rdstrm(rdbuf), wtstrm(wtbuf);
 
-	grq_ras.Encode(wtstrm);
-	wtstrm.CompleteEncoding();
+    grq_ras.Encode(wtstrm);
+    wtstrm.CompleteEncoding();
 
-	PUDPSocket sock(gk_port);
-	if (!sock.Connect(gk_addr))
-	{
-		cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		_exit(1);
-	}
+    PUDPSocket sock(gk_port);
+    if (!sock.Connect(gk_addr)) {
+        cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
+        sock.Close();
+        _exit(1);
+    }
 
-	sock.Write(wtstrm.GetPointer(), wtstrm.GetSize());
+    sock.Write(wtstrm.GetPointer(), wtstrm.GetSize());
 
-	sock.SetReadTimeout(GetReadTimeout());
-	if (!sock.ReadFrom(rdstrm.GetPointer(), rdstrm.GetSize(), gk_addr, gk_port)) {
-		cout << "CRITICAL - Timeout while waiting for GCF/GRJ" << endl;
-		_exit(1);
-	}
+    sock.SetReadTimeout(GetReadTimeout());
+    if (!sock.ReadFrom(rdstrm.GetPointer(), rdstrm.GetSize(), gk_addr, gk_port)) {
+        cout << "CRITICAL - Timeout while waiting for GCF/GRJ" << endl;
+        _exit(1);
+    }
 
-	grq_rpl.Decode(rdstrm);
-	sock.Close();
+    grq_rpl.Decode(rdstrm);
+    sock.Close();
 
-	cout << "OK - " << grq_rpl.GetTagName() << " from " << gk_addr << endl;
-	_exit(0);
+    cout << "OK - " << grq_rpl.GetTagName() << " from " << gk_addr << endl;
+    _exit(0);
 }
 
 void Client::SendLRQ()
@@ -172,8 +171,8 @@ void Client::SendLRQ()
 
     lrq.m_requestSeqNum.SetValue(1);
 
-	// set replyAddress (mandatory)
-	lrq.m_replyAddress = SocketToH225TransportAddr(my_addr, gk_port);
+    // set replyAddress (mandatory)
+    lrq.m_replyAddress = SocketToH225TransportAddr(my_addr, gk_port);
 
     lrq.m_destinationInfo.SetSize(1);
     H323SetAliasAddress(PString("check_h323"), lrq.m_destinationInfo[0]);
@@ -187,27 +186,27 @@ void Client::SendLRQ()
     lrq_ras.Encode(wtstrm);
     wtstrm.CompleteEncoding();
 
-   PUDPSocket sock(gk_port);
-   if (!sock.Connect(gk_addr)) {
-		cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
-		sock.Close();
-		_exit(1);
+    PUDPSocket sock(gk_port);
+    if (!sock.Connect(gk_addr)) {
+        cout << "CRITICAL - Can not connect to the gatekeeper " << gk_addr << endl;
+        sock.Close();
+        _exit(1);
     }
 
     sock.Write(wtstrm.GetPointer(), wtstrm.GetSize());
 
-	// wait for LCF / LRJ
-	sock.SetReadTimeout(GetReadTimeout());
+    // wait for LCF / LRJ
+    sock.SetReadTimeout(GetReadTimeout());
     sock.ReadFrom(rdstrm.GetPointer(), rdstrm.GetSize(), gk_addr, gk_port);
     lrq_rpl.Decode(rdstrm);
     sock.Close();
 
     if ((lrq_rpl.GetTag() == H225_RasMessage::e_locationConfirm)
-		|| (lrq_rpl.GetTag() == H225_RasMessage::e_locationReject)) {
-		cout << "OK - " << lrq_rpl.GetTagName() << " from " << gk_addr << endl;
-		_exit(0);
-	} else {
-		cout << "CRITICAL - no answer" << endl;
-		_exit(1);
-	}
+        || (lrq_rpl.GetTag() == H225_RasMessage::e_locationReject)) {
+        cout << "OK - " << lrq_rpl.GetTagName() << " from " << gk_addr << endl;
+        _exit(0);
+    } else {
+        cout << "CRITICAL - no answer" << endl;
+        _exit(1);
+    }
 }
